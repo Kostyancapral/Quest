f7ef5d27469066c2e6d2e02598d27634ac1d3b1a
Kostya Krutenuk <capralkoooooo@gmail.com>
2017-02-05 21:30:22 +0200

    third

--- 1.2/menu/menu/Program.cs
+++ 1.2/menu/menu/Program.cs
@@ -11,8 +11,8 @@ namespace menu
     {
         static void Main(string[] args)
         {
-            int menu = -1;
-            while (menu != 0)
+            string menu = "-1";
+            while (menu != "0")
             {
                 Console.Clear();
                 Console.WriteLine("Выберите задание, рузультат которого вы хотите увидеть");
@@ -25,34 +25,34 @@ namespace menu
                 Console.WriteLine("7 - Гуси и кролики");
                 Console.WriteLine("8 - Пароль");
                 Console.WriteLine("0 - Выход");
-                menu = Convert.ToInt32(Console.ReadLine());
+                menu = Console.ReadLine();
                 switch (menu)
                 {
-                    case 1:
+                    case "1":
                         Process.Start("1\\1 multiplication table.exe");
                         break;
-                    case 2:
+                    case "2":
                         Process.Start("2\\2 number of digits.exe");
                         break;
-                    case 3:
+                    case "3":
                         Process.Start("3\\3 extrasensory.exe");
                         break;
-                    case 4:
+                    case "4":
                         Process.Start("4\\4 pow(2).exe");
                         break;
-                    case 5:
+                    case "5":
                         Process.Start("5\\5 arithmetic average.exe");
                         break;
-                    case 6:
+                    case "6":
                         Process.Start("6\\6 random arithmetik average.exe");
                         break;
-                    case 7:
+                    case "7":
                         Process.Start("7\\7 gangers and rabbits.exe");
                         break;
-                    case 8:
+                    case "8":
                         Process.Start("8\\8 password.exe");
                         break;
-                    case 0:
+                    case "0":
                         break;
                 }
             }
